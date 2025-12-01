f0ebbed4ebf99219d2f7b5dbbf587c5f78617138
rogergilfg <rogergil.fg@gmail.com>
2025-12-01 11:59:11 +0100

    01/12/2025

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,11 +11,19 @@ public class PlayerController : MonoBehaviour
 
     private Animator animator;
     private PlayerInput playerInput;
+    private Rigidbody rb;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float sensibility;
+    [SerializeField]
+    private Transform followTarget;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -23,6 +31,15 @@ public class PlayerController : MonoBehaviour
         Vector2 leftStickInput = playerInput.actions[MOVEMENT_ACTION_NAME].ReadValue<Vector2>();
         animator.SetFloat(ANIMATOR_HORIZONTAL, leftStickInput.x);
         animator.SetFloat(ANIMATOR_VERTICAL, leftStickInput.y);
+        Vector3 movement = ((transform.forward * leftStickInput.y) + (transform.right * leftStickInput.x)) * speed;
+        rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
+    }
+
+    private void LateUpdate()
+    {
+        Vector2 lookInput = playerInput.actions["Look"].ReadValue<Vector2>();
+        followTarget.localEulerAngles += new Vector3(lookInput.y*sensibility*Time.deltaTime, 0, 0);
+        transform.eulerAngles += new Vector3(0, lookInput.x * sensibility * Time.deltaTime, 0);
     }
 
     public void Shoot(InputAction.CallbackContext context)
